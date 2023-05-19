dc268ba2c925aa15bae56feacd7b001f31865ee7
NenoyHeker <106822874+NenoyHeker@users.noreply.github.com>
2023-05-19 12:33:40 -0600

    Modelo y controlador
    Creacion de un test que incluye el modelo, el controlador y el archivo de rutas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 'use strict'
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
+
+
+
+
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Fato kato tactico desplegado\n");
 });
-
-//tal vez codigo temporal?
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = "mongodb+srv://estorenplay:oo3Pf9vr46CX3Sek@cluster0.jasnhyh.mongodb.net/?retryWrites=true&w=majority";
@@ -23,10 +27,27 @@ MongoClient.connect(uri,{useUnifiedTopology:true},(err,client) => {
     console.log("Fatito katito conectado correctamente a MongoDB")
 });
 
-//fin del codigo temporal???
-
 server.listen(port,hostname,()=>{
     console.log(`Katito fatito corriendo en http://${hostname}:${port}/`);
 });
 
+const dbName = "dbtest01";
+var url =
 
+//Carga del body parser, analizador de bodys a travez de la URL
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+//Peticiones -> JSON
+
+app.use(bodyParser.json());
+
+//Activar CORS para permitir peticiones AJAX y HTTP desde el frontend
+app.use((req,res) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETED');
+    res.header('Allow','GET,POST, OPTIONS, PUT, DELETE');
+    next();
+});
